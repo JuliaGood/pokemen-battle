3276d9ab7777e707813ec559b8e77c3cde02616f
JuliaGood <gudak.yuliya@gmail.com>
2021-01-27 20:14:43 +0200

    Pokegame component was added and random HANDs

---
+++ src/Pokegame.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import Pokedex from './Pokedex';
+
+class Pokegame extends Component {
+  static defaultProps = {
+    pokemens: [
+      {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
+      {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
+      {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
+      {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
+      {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
+      {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
+      {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
+      {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
+    ]
+  }
+  render() {
+    let hand1 = [];
+    let hand2 = [...this.props.pokemens];
+    while(hand1.length < hand2.length) {
+      let randIndex = Math.floor(Math.random() * hand2.length);
+      let randPokemen = hand2.splice(randIndex, 1)[0];
+      hand1.push(randPokemen);
+    }
+    console.log(hand1);
+    console.log(hand2);
+    return (
+      <div>
+        <Pokedex pokemens={hand1} />
+        <Pokedex pokemens={hand2} />
+      </div>
+    )
+  }
+}
+
+export default Pokegame;
